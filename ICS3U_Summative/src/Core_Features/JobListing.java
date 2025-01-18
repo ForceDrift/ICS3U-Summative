a51d28049d2fdca9143355ef2d279b225b596a24
ForceDrift <133434396+ForceDrift@users.noreply.github.com>
2025-01-17 23:21:54 -0500

    Update JobListing.java

--- ICS3U_Summative/src/Core_Features/JobListing.java
+++ ICS3U_Summative/src/Core_Features/JobListing.java
@@ -1,11 +1,12 @@
 package Core_Features;
 
+//this is the job listing class
 public class JobListing {
 	  private String jobId;
       private String title;
       private String description;
       private double budget;
-
+//initialize constructor
       public JobListing(String jobId, String title, String description, double budget) {
           this.jobId = jobId;
           this.title = title;
@@ -13,6 +14,7 @@ public class JobListing {
           this.budget = budget;
       }
 
+	//getter and setter methods below
       public String getJobId() {
           return jobId;
       }
